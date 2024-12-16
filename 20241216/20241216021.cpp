54b588b9bf7961bf2137c1be14d4d5f99b14a34d
Penici11in. <hiddenqin@gmail.com>
2024-12-16 16:33:57 +0800

    Add files via upload

---
+++ 20241216/20241216021.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+bool isLeapYear(int year){
+  return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+}
+
+int main(){
+  int year;
+  cin >> year;
+  cout << (isLeapYear(year) ? "yes" : "no") << endl;
+  return 0;
+}
